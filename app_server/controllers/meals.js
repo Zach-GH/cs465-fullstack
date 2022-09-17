47ba6453596c38c89c1e817398b25d31d52956c4
Zachary Meisner <prettyzucchini@Zacharys-MacBook-Air.local>
2022-09-17 17:12:17 -0400

    pushing module 3 changes

--- app_server/controllers/meals.js
+++ app_server/controllers/meals.js
@@ -1,7 +1,11 @@
+var fs = require('fs');
+
+var foods = JSON.parse(fs.readFileSync('../data/foods.json', 'utf8'));
+
 /* GET meals view */
 const meals = (req, res) => {
     pageTitle = process.env.npm_package_description + " - Meals";
-    res.render('meals', {title: pageTitle });
+    res.render('meals', {title: pageTitle, foods});
 };
 
 module.exports = {
--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -1,7 +1,11 @@
+var fs = require('fs');
+
+var stays = JSON.parse(fs.readFileSync('../data/stays.json', 'utf8'));
+
 /* GET rooms view */
 const rooms = (req, res) => {
     pageTitle = process.env.npm_package_description + " - Rooms";
-    res.render('rooms', {title: pageTitle });
+    res.render('rooms', {title: pageTitle, stays });
 };
 
 module.exports = {
